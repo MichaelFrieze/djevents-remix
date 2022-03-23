3ed87f5de6e4bc83cb8970f9d9edc3bfe0b4d259
Michael Frieze <michaelfrieze@icloud.com>
2022-03-22 21:09:19 -0400

    updated AddEventRoute by doing forms the Remix way

--- app/routes/events/add.jsx
+++ app/routes/events/add.jsx
@@ -1,6 +1,4 @@
-import { Link, useLoaderData } from 'remix';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, Form, redirect } from 'remix';
 import { API_URL } from '~/config/index';
 import addEventStyles from '~/styles/routes/events/add.css';
 
@@ -13,144 +11,78 @@ export let links = () => {
   ];
 };
 
-export let loader = async () => {
-  return API_URL;
+export let leader = async () => {};
+
+export let action = async ({ request }) => {
+  let formData = await request.formData();
+  let values = Object.fromEntries(formData);
+
+  let strapiAddEventData = {
+    data: {
+      ...values,
+      slug: values.name.toLowerCase().replace(/'/g, '').replace(/\s/g, '-'),
+    },
+  };
+
+  let res = await fetch(`${API_URL}/api/events`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(strapiAddEventData),
+  });
+
+  if (!res.ok) {
+    throw new Error('Something Went Wrong');
+  }
+
+  let evt = await res.json();
+
+  return redirect(`/events/${evt.data.attributes.slug}`);
 };
 
 export default function AddEventRoute() {
-  let navigate = useNavigate();
-  let strapiURL = useLoaderData();
-
-  let [values, setValues] = useState({
-    name: '',
-    performers: '',
-    venue: '',
-    address: '',
-    date: '',
-    time: '',
-    description: '',
-  });
-
-  let handleSubmit = async (e) => {
-    e.preventDefault();
-    // Validation
-    let hasEmptyFields = Object.values(values).some(
-      (element) => element === ''
-    );
-    if (hasEmptyFields) {
-      alert('Please fill in all fields');
-      return;
-    }
-
-    let strapiAddEventData = {
-      data: {
-        ...values,
-        slug: values.name.toLowerCase().replace(/'/g, '').replace(/\s/g, '-'),
-      },
-    };
-
-    let res = await fetch(`${strapiURL}/api/events`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(strapiAddEventData),
-    });
-    if (!res.ok) {
-      throw new Error('Something Went Wrong');
-    } else {
-      let evt = await res.json();
-      console.log(evt);
-      navigate(`/events/${evt.data.attributes.slug}`);
-    }
-  };
-
-  let handleInputChange = (e) => {
-    let { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
-
   return (
     <>
       <Link to="/events">Go Back</Link>
       <h1>Add Event</h1>
-      <form onSubmit={handleSubmit} className="form">
+      <Form method="post" className="form">
         <div className="grid">
           <div>
             <label htmlFor="name">Event Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={values.name}
-              onChange={handleInputChange}
-            />
+            <input type="text" id="name" name="name" />
           </div>
           <div>
             <label htmlFor="performers">Performers</label>
-            <input
-              type="text"
-              name="performers"
-              id="performers"
-              value={values.performers}
-              onChange={handleInputChange}
-            />
+            <input type="text" name="performers" id="performers" />
           </div>
           <div>
             <label htmlFor="venue">Venue</label>
-            <input
-              type="text"
-              name="venue"
-              id="venue"
-              value={values.venue}
-              onChange={handleInputChange}
-            />
+            <input type="text" name="venue" id="venue" />
           </div>
           <div>
             <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              name="address"
-              id="address"
-              value={values.address}
-              onChange={handleInputChange}
-            />
+            <input type="text" name="address" id="address" />
           </div>
           <div>
             <label htmlFor="date">Date</label>
-            <input
-              type="date"
-              name="date"
-              id="date"
-              value={values.date}
-              onChange={handleInputChange}
-            />
+            <input type="date" name="date" id="date" />
           </div>
           <div>
             <label htmlFor="time">Time</label>
-            <input
-              type="text"
-              name="time"
-              id="time"
-              value={values.time}
-              onChange={handleInputChange}
-            />
+            <input type="text" name="time" id="time" />
           </div>
         </div>
 
         <div>
           <label htmlFor="description">Event Description</label>
-          <textarea
-            type="text"
-            name="description"
-            id="description"
-            value={values.description}
-            onChange={handleInputChange}
-          ></textarea>
+          <textarea type="text" name="description" id="description"></textarea>
         </div>
 
-        <input type="submit" value="Add Event" className="btn" />
-      </form>
+        <button className="btn" type="submit">
+          Add Event
+        </button>
+      </Form>
     </>
   );
 }
