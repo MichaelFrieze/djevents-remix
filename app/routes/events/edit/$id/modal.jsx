f2a6427adcd2334546d6dd6bdefe6f550f67c076
Michael Frieze <michaelfrieze@icloud.com>
2022-03-25 15:08:49 -0400

    image upload is working and so is associating the image with a collection type

--- app/routes/events/edit/$id/modal.jsx
+++ app/routes/events/edit/$id/modal.jsx
@@ -11,6 +11,7 @@ import {
   unstable_createMemoryUploadHandler,
 } from 'remix';
 import { FaTimes } from 'react-icons/fa';
+import { API_URL } from '~/config/index';
 import modalStyles from '~/styles/modal.css';
 import formStyles from '~/styles/form.css';
 
@@ -29,14 +30,43 @@ export let links = () => {
 
 export let action = async ({ request }) => {
   const uploadHandler = unstable_createMemoryUploadHandler({
-    maxFileSize: 500_000,
+    maxFileSize: 5_000_000,
   });
 
   let formData = await unstable_parseMultipartFormData(request, uploadHandler);
+  let { eventID } = Object.fromEntries(formData);
+  let formImage = formData.get('image');
 
-  let image = formData.get('image');
+  let uploadData = new FormData();
+  uploadData.append('files', formImage);
+  uploadData.append('ref', 'api::event.event');
+  uploadData.append('refId', eventID);
+  uploadData.append('field', 'image');
 
-  return image;
+  // let res = await fetch(`${API_URL}/api/events/${eventID}?populate=image`, {
+  //   method: 'PUT',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     data: {
+  //       ...fields,
+  //     },
+  //   }),
+  // });
+
+  let res = await fetch(`${API_URL}/api/upload`, {
+    method: 'POST',
+    body: uploadData,
+  });
+
+  return {
+    formData,
+    eventID,
+    formImage,
+    uploadData,
+    res,
+  };
 };
 
 export let loader = async ({ params, request }) => {
@@ -45,7 +75,14 @@ export let loader = async ({ params, request }) => {
 
 export default function EditEventModalRoute() {
   let loaderData = useLoaderData();
+  // let { formData, eventID, formImage, uploadData, res } = useActionData();
+  // console.log('Form Data: ', formData);
+  // console.log('Event ID: ', eventID);
+  // console.log('form Image: ', formImage);
+  // console.log('Upload Data: ', uploadData);
+  // console.log('Res: ', res);
   let actionData = useActionData();
+  console.log(actionData);
 
   return (
     <>
@@ -64,6 +101,7 @@ export default function EditEventModalRoute() {
                 <div className="file">
                   <input type="file" id="image" name="image" />
                 </div>
+                <input type="hidden" name="eventID" value={loaderData.id} />
                 <button className="btn btn-modal" type="submit">
                   Upload
                 </button>
