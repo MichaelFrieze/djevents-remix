157e16eff57b188610afbdefee15a0e5a11956f8
Michael Frieze <michaelfrieze@icloud.com>
2022-04-05 21:44:04 -0400

    added basic error handling in the root

--- app/root.jsx
+++ app/root.jsx
@@ -5,6 +5,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from 'remix';
 import { getUser } from '~/utils/session.server.js';
 import { Layout, links as layoutLinks } from '~/components/layout';
@@ -39,19 +40,52 @@ export let loader = async ({ request }) => {
 
 export default function App() {
   return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+function Document({ children }) {
+  return (
     <html lang="en">
       <head>
         <Meta />
         <Links />
       </head>
       <body>
-        <Layout>
-          <Outlet />
-        </Layout>
+        <Layout>{children}</Layout>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document>
+      <div className="error-container">
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </div>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  console.error(error);
+
+  return (
+    <Document>
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
   );
 }
